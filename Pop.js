85d989edc260df1bf4ce86ac2ceefc2c62942ab8
downloadsuper <84514270+downloadsuper@users.noreply.github.com>
2026-06-22 20:26:17 +0100

    Add files via upload

---
+++ Pop.js
@@ -0,0 +1,9 @@
+{
+"enabled": true,
+"autoStart": "On",
+"link": "https://app.hawktrk.com/sl?id=6a2050db46d3cf0d62f32aa4&pid=2&sub2=u787576&sub6=s2smartLink&sub5=s1SUBID1HERE",
+"image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiHrbH43y2Ehbfrc1F6YBScwR6My829Cc2tCP9b7Lugo2ypIeerSbAI5g&s=10",
+ "ctaText": "GET IT NOW",
+"closeAfter": 6,
+"urgencySeconds": 600
+}
