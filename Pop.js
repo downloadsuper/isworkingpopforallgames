f0c60b110ffd7860dc65d533bcb95b32d98fafcd
downloadsuper <84514270+downloadsuper@users.noreply.github.com>
2026-07-02 17:04:28 +0100

    Add files via upload

--- Pop.js
+++
@@ -1,9 +0,0 @@
-  window.CPA_CONFIG = {
-     "enabled": true,
-     "autoStart": "On",
-     "link": "https://app.hawktrk.com/sl?id=6a2050db46d3cf0d62f32aa4&pid=2&sub2=u787576&sub6=s2smartLink&sub5=s1SUBID1HERE",
-     "image": "https://cdn.thepcenthusiast.com/wp-content/uploads/2026/01/Poki-How-It-Builds-The-Largest-Browser-Gaming-Playground.jpg",
-     "ctaText": "GET IT NOW",
-     "closeAfter": 6,
-     "urgencySeconds": 600
-   };
